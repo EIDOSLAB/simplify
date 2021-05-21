9250dd8a68edc902c1b1c2da57b2014dc952af93
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2021-05-21 16:39:01 +0200

    fix device

--- conv.py
+++ conv.py
@@ -5,7 +5,8 @@ class ConvB(nn.Conv2d):
     @staticmethod
     def from_conv(module: nn.Conv2d, bias):
         module.__class__ = ConvB
-        setattr(module, 'bf', bias.to(module.weight.device))
+        setattr(module, 'bf', bias)
+        module.bf = module.bf.to(module.weight.device)
         return module
         
     def forward(self, x):
