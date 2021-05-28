ae43ea1cf51b66ec4b1d6bcf972f104048881291
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2021-05-28 15:44:11 +0200

    update get_pinned_out to couple only downsamples/conv

--- utils.py
+++ utils.py
@@ -16,36 +16,22 @@ def set_seed(seed):
     torch.manual_seed(seed)
 
 def get_pinned_out(model):
-    pinned_out = []
+    pinned_out = {}
 
     if isinstance(model, ResNet):
-        pinned_out = {'conv1': []}
-        last_module = [('conv1', model.conv1)]
-
-        all_modules = []
         for name, module in model.named_modules():
             if not isinstance(module, (BasicBlock, Bottleneck)):
                 continue
 
+            block_last = (f'{name}.conv2', module.conv2)
+            if isinstance(module, Bottleneck):
+                block_last = (f'{name}.conv3', module.conv3)
+            
             if module.downsample is not None:
-                all_modules.append(module.downsample[0])
-
-            if isinstance(module, BasicBlock):
-                all_modules.append(module.conv2)
-            elif isinstance(module, Bottleneck):
-                all_modules.append(module.conv3)
-
-        pinned_out['conv1'] = all_modules
-        for name, module in model.named_modules():
-            if not isinstance(module, (BasicBlock, Bottleneck)):
-               continue
-
-            if module.downsample is not None:
-                pinned_out[f'{name}.downsample.0'] = all_modules
-
-            if isinstance(module, BasicBlock):
-                pinned_out[f'{name}.conv2'] = all_modules
+                pinned_out[block_last[0]] = module.downsample[0]
+                pinned_out[f'{name}.downsample.0'] = block_last[1]
+            
             else:
-                pinned_out[f'{name}.conv3'] = all_modules   
+                pinned_out[block_last[0]] = None
 
     return pinned_out
