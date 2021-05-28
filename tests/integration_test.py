4673e5984e0422e1f0eb1464a6c8b88f9d5042d1
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2021-05-28 10:54:26 +0200

    add residual concat tests

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -15,8 +15,47 @@ class IntegrationTest(unittest.TestCase):
     def setUp(self):
         set_seed(3)
 
-    def test_simplification(self):
+    @torch.no_grad()
+    def test_cat_residual_conv(self):
+        class Residual(nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.module0 = nn.Conv2d(3, 32, 3, stride=1, padding=1, bias=True)
+                self.module1 = nn.Conv2d(32, 64, 3, stride=1, padding=1, bias=True)
+                self.module2 = nn.Conv2d(32, 64, 3, stride=1, padding=1, bias=True)
+                self.module3 = nn.Conv2d(64*2, 64, 3, stride=1, padding=1, bias=True)
+                self.relu = nn.ReLU()
+                
+                prune.random_structured(self.module0, 'weight', amount=0.5, dim=0)
+                prune.remove(self.module0, 'weight')
+                
+                prune.random_structured(self.module1, 'weight', amount=0.4, dim=0)
+                prune.remove(self.module1, 'weight')
+                
+                prune.random_structured(self.module2, 'weight', amount=0.8, dim=0)
+                prune.remove(self.module2, 'weight')
+            
+                self.a = None
+                self.b = None
+                self.c = None
+            
+            def forward(self, x):
+                x = self.relu(self.module0(x))
+                self.a = self.module1(x)
+                self.b = self.module2(x)
+                self.c = torch.cat((self.a, self.b), dim=1)
+                return self.module3(self.relu(self.c))
+        
+        residual = Residual()
+        x = torch.randn((10, 3, 224, 224))
+        
+        y_src = residual(x)
+        simplify.simplify(residual, torch.zeros((1, 3, 224, 224)), [])
+        y_prop = residual(x)
+        
+        self.assertTrue(torch.allclose(y_src, y_prop, atol=1e-6))
 
+    def test_simplification(self):
         @torch.no_grad()
         def test_arch(arch, x, pretrained=False):
             model = arch(pretrained, progress=False)
