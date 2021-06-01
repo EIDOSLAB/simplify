7623823baee9e4f2ead7f65777a0977a1bd2748d
Andrea Bragagnolo <andr.bragagnolo@gmail.com>
2021-06-01 11:31:58 +0200

    added no_grad

--- get_steps_status.py
+++ get_steps_status.py
@@ -1,64 +1,98 @@
 import torch
 from tabulate import tabulate
+from torch import nn
+from torch.nn.utils import prune
+from torchvision.models import SqueezeNet
 
 import fuser
 import utils
 from simplify import __propagate_bias, __remove_zeroed
 from tests.benchmark_models import models
 
+
+def get_mark(passed):
+    if isinstance(passed, bool):
+        return ":heavy_check_mark:" if passed else ":x:"
+    else:
+        return str(passed)
+
+
 if __name__ == '__main__':
     utils.set_seed(3)
     
-    x = torch.randint(0, 256, (256, 3, 224, 224))
+    x = torch.randint(0, 256, (1, 3, 224, 224))
     x = x.float() / 255.
+    input = torch.randint(0, 256, (256, 3, 224, 224))
+    input = input.float() / 255.
     
-    table = []
-    for architecture in models:
-        print(architecture.__name__)
-        model = architecture(True, progress=False)
-        model.eval()
-        y_src = model(x)
-        
-        print("BatchNorm Folding")
-        try:
-            model = fuser.fuse(model)
-            exception = None
-        except Exception as e:
-            exception = e.__class__
-        y_dest = model(x)
-        passed = torch.equal(y_src.argmax(dim=1), y_dest.argmax(dim=1))
-        table.append([architecture.__name__, "BatchNorm Folding", ":heavy_check_mark:" if passed else ":x:",
-                      "" if exception is None else exception])
-        if not passed:
-            continue
-        
-        pinned_out = utils.get_pinned_out(model)
-        
-        print("Bias Propagation")
-        try:
-            __propagate_bias(model, x, pinned_out)
-            exception = None
-        except Exception as e:
-            exception = e.__class__
-        y_dest = model(x)
-        passed = torch.equal(y_src.argmax(dim=1), y_dest.argmax(dim=1))
-        table.append([architecture.__name__, "Bias Propagation", ":heavy_check_mark:" if passed else ":x:",
-                      "" if exception is None else exception])
-        if not passed:
-            continue
-        
-        print("Simplification")
-        try:
-            __remove_zeroed(model, pinned_out)
-            exception = None
-        except Exception as e:
-            exception = e.__class__
-        y_dest = model(x)
-        passed = torch.equal(y_src.argmax(dim=1), y_dest.argmax(dim=1))
-        table.append([architecture.__name__, "Simplification", ":heavy_check_mark:" if passed else ":x:",
-                      "" if exception is None else exception])
-        if not passed:
-            continue
-    
-    table = tabulate(table, headers=['Architecture', 'Step', 'Pass', 'Exception'], tablefmt='github')
+    with torch.no_grad():
+        table = []
+        for architecture in models:
+            print(architecture.__name__)
+            if architecture.__name__ in ["shufflenet_v2_x1_5", "shufflenet_v2_x2_0", "mnasnet0_75", "mnasnet1_3"]:
+                pretrained = False
+            else:
+                pretrained = True
+            model = architecture(pretrained)
+            model.eval()
+            
+            for name, module in model.named_modules():
+                if isinstance(model, SqueezeNet) and 'classifier.1' in name:
+                    continue
+                
+                if isinstance(module, nn.Conv2d):
+                    prune.random_structured(module, 'weight', amount=0.8, dim=0)
+                    prune.remove(module, 'weight')
+            
+            y_src = model(input)
+            
+            print("BatchNorm Folding")
+            try:
+                model = fuser.fuse(model)
+                model.eval()
+                exception = None
+                y_dest = model(input)
+                passed_bf = torch.equal(y_src.argmax(dim=1), y_dest.argmax(dim=1))
+                print(
+                    f'Correct predictions: {torch.eq(y_src.argmax(dim=1), y_dest.argmax(dim=1)).sum()}/{y_dest.shape[0]}')
+            except Exception as e:
+                passed_bf = "exception"
+            
+            if isinstance(passed_bf, bool) and passed_bf:
+                
+                pinned_out = utils.get_pinned_out(model)
+                
+                print("Bias Propagation")
+                try:
+                    __propagate_bias(model, x, pinned_out)
+                    model.eval()
+                    exception = None
+                    y_dest = model(input)
+                    passed_bp = torch.equal(y_src.argmax(dim=1), y_dest.argmax(dim=1))
+                    print(
+                        f'Correct predictions: {torch.eq(y_src.argmax(dim=1), y_dest.argmax(dim=1)).sum()}/{y_dest.shape[0]}')
+                except Exception as e:
+                    passed_bp = "exception"
+                
+                if isinstance(passed_bp, bool) and passed_bp:
+                    
+                    print("Simplification")
+                    try:
+                        __remove_zeroed(model, pinned_out)
+                        model.eval()
+                        exception = None
+                        y_dest = model(input)
+                        passed_simp = torch.equal(y_src.argmax(dim=1), y_dest.argmax(dim=1))
+                        print(
+                            f'Correct predictions: {torch.eq(y_src.argmax(dim=1), y_dest.argmax(dim=1)).sum()}/{y_dest.shape[0]}')
+                    except Exception as e:
+                        passed_simp = "exception"
+                else:
+                    passed_simp = "skipped"
+            else:
+                passed_bp, passed_simp = "skipped", "skipped"
+            
+            table.append([architecture.__name__, get_mark(passed_bf), get_mark(passed_bp), get_mark(passed_simp)])
+    table = tabulate(table, headers=['Architecture', 'BatchNorm Folding', 'Bias Propagation', 'Simplification'],
+                     tablefmt='github')
     print(table)
--- tests/benchmark_models.py
+++ tests/benchmark_models.py
@@ -8,11 +8,20 @@ from torchvision.models.mobilenet import mobilenet_v2, mobilenet_v3_small, mobil
 from torchvision.models.resnet import resnet18, resnet34, resnet50, resnet101, resnet152
 from torchvision.models.shufflenetv2 import shufflenet_v2_x0_5, shufflenet_v2_x1_0, shufflenet_v2_x1_5, \
     shufflenet_v2_x2_0
-from torchvision.models.squeezenet import SqueezeNet, squeezenet1_0, squeezenet1_1
+from torchvision.models.squeezenet import squeezenet1_0, squeezenet1_1
 from torchvision.models.vgg import vgg11, vgg11_bn, vgg13, vgg13_bn, vgg16, vgg16_bn, vgg19, vgg19_bn
 
-models = [alexnet, resnet18, resnet34, resnet50, resnet101, resnet152, vgg11, vgg11_bn, vgg13, vgg13_bn, vgg16,
-          vgg16_bn, vgg19, vgg19_bn, SqueezeNet, squeezenet1_0, squeezenet1_1, densenet121, densenet161, densenet169,
-          densenet201, inception_v3, googlenet, shufflenet_v2_x0_5, shufflenet_v2_x1_0, shufflenet_v2_x1_5,
-          shufflenet_v2_x2_0, mobilenet_v2, mobilenet_v3_small, mobilenet_v3_large, resnext50_32x4d, resnext101_32x8d,
-          wide_resnet50_2, wide_resnet101_2, mnasnet0_5, mnasnet0_75, mnasnet1_0, mnasnet1_3]
+models = [
+    alexnet,
+    vgg11, vgg11_bn, vgg13, vgg13_bn, vgg16, vgg16_bn, vgg19, vgg19_bn,
+    resnet18, resnet34, resnet50, resnet101, resnet152,
+    squeezenet1_0, squeezenet1_1,
+    densenet121, densenet161, densenet169, densenet201,
+    inception_v3,
+    googlenet,
+    shufflenet_v2_x0_5, shufflenet_v2_x1_0, shufflenet_v2_x1_5, shufflenet_v2_x2_0,
+    mobilenet_v2, mobilenet_v3_small, mobilenet_v3_large,
+    resnext50_32x4d, resnext101_32x8d,
+    wide_resnet50_2, wide_resnet101_2,
+    mnasnet0_5, mnasnet0_75, mnasnet1_0, mnasnet1_3
+]
