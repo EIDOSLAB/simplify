54099a8bd7def5fc029a6204f1cd5ff8c74150a8
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2021-06-01 11:44:57 +0200

    add MobileNetV3 pinned

--- utils.py
+++ utils.py
@@ -2,7 +2,10 @@ import os
 import random
 
 import numpy as np
+from numpy.lib import isin
 import torch
+from torchvision.models.mobilenetv3 import MobileNetV3
+from torchvision.models.mobilenetv3 import InvertedResidual as InvertedResidual_MobileNetV3
 from torchvision.models.resnet import ResNet, BasicBlock, Bottleneck
 
 
@@ -34,5 +37,15 @@ def get_pinned_out(model):
 
             if module.downsample is not None:
                 pinned_out.append(f'{name}.downsample.0')
+
+    if isinstance(model, MobileNetV3):
+        pinned_out = ['features.0.0']
+
+        for name, module in model.named_modules():
+            if isinstance(module, InvertedResidual_MobileNetV3):
+                block_len = len(module.block)
+                pinned_out.append(f'{name}.block.{block_len-1}.0')
     
+    print('Pinned layers:', pinned_out)
+
     return pinned_out
