28d2f4dbe41ac88e8f705ae34fe9b31b29433774
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2021-06-03 09:23:39 +0200

    fix mobilenetv3 pinned

--- utils.py
+++ utils.py
@@ -4,8 +4,10 @@ import random
 import numpy as np
 from numpy.lib import isin
 import torch
-from torchvision.models.mobilenetv3 import MobileNetV3
-from torchvision.models.mobilenetv3 import InvertedResidual as InvertedResidual_MobileNetV3
+import torch.nn as nn
+from torchvision.models.mobilenetv3 import MobileNetV3, InvertedResidual as InvertedResidual_MobileNetV3 
+from torchvision.models.mobilenetv2 import MobileNetV2, InvertedResidual as InvertedResidual_MobileNetV2
+
 from torchvision.models.resnet import ResNet, BasicBlock, Bottleneck
 
 
@@ -39,13 +41,33 @@ def get_pinned_out(model):
                 pinned_out.append(f'{name}.downsample.0')
 
     if isinstance(model, MobileNetV3):
-        pinned_out = ['features.0.0']
+        pinned_out = ['features.0.0', f'features.{len(model.features)-1}.0']
+
+        last_module = None
 
         for name, module in model.named_modules():
+            if isinstance(module, nn.Conv2d):
+                if module.groups > 1 and last_module is not None:
+                    pinned_out.append(last_module)
+                last_module = name
+
             if isinstance(module, InvertedResidual_MobileNetV3):
                 block_len = len(module.block)
+                #if module.use_res_connect:
                 pinned_out.append(f'{name}.block.{block_len-1}.0')
-    
-    print('Pinned layers:', pinned_out)
 
+            if 'fc' in name:
+                pinned_out.append(name)
+
+    if isinstance(model, MobileNetV2):
+        pinned_out = []
+
+        last_module = None
+        for name, module in model.named_modules():
+            if isinstance(module, nn.Conv2d):
+                if module.groups > 1 and last_module is not None:
+                    pinned_out.append(last_module)
+                last_module = name
+    
     return pinned_out
+
