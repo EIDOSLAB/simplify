01ec7bd0272187f51f89fca37b0a3fbfe4e4e3e2
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2021-06-08 16:07:05 +0200

    set pruning amount to 0.8

--- build_steps_table.py
+++ build_steps_table.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
                     continue
                 
                 if isinstance(module, nn.Conv2d):
-                    prune.random_structured(module, 'weight', amount=0.5, dim=0)
+                    prune.random_structured(module, 'weight', amount=0.8, dim=0)
                     prune.remove(module, 'weight')
                     
                 if isinstance(module, nn.BatchNorm2d):
