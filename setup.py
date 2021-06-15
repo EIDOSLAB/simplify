5d50daf7f38f338383e05b2cc05c086bacc34e60
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2021-06-15 12:10:06 +0200

    fix license

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name='torch-simplify',
-    version='0.0.9',
+    version='1.0.0',
     description='Simplification of pruned models for accelerated inference',
     long_description=README,
     long_description_content_type='text/markdown',
     url='https://github.com/EIDOSlab/simplify',
     author='EIDOSlab',
     author_email='eidoslab@di.unito.it',
-    license='BSD 3',
+    license='BSD 3-Clause',
     packages=find_packages(exclude=('profile', '.github', '.idea')),
     zip_safe=False,
     install_requires=[
--- simplify/__init__.py
+++ simplify/__init__.py
@@ -8,7 +8,7 @@ from .fuse import fuse
 from .propagate import propagate_bias
 from .remove import remove_zeroed
 
-__version__ = "0.0.9"
+__version__ = "1.0.0"
 
 
 def simplify(model: nn.Module, x: torch.Tensor, pinned_out: List = None, bn_folding: List = None) -> nn.Module:
