b01ed896b9be7be65caf810946c2fea1cb2480e1
Andrea Bragagnolo <andr.bragagnolo@gmail.com>
2021-07-02 15:07:16 +0200

    added expand to BatchNormExpand

--- simplify/layers.py
+++ simplify/layers.py
@@ -9,7 +9,7 @@ class ConvB(nn.Conv2d):
         module.__class__ = ConvB
         module.register_parameter('bf', torch.nn.Parameter(bias))
         return module
-
+    
     def forward(self, x):
         x = super().forward(x)
         return x + self.bf
@@ -19,23 +19,23 @@ class ConvExpand(nn.Conv2d):
     @staticmethod
     def from_conv(module: nn.Conv2d, idxs, bias):
         module.__class__ = ConvExpand
-
+        
         module.register_buffer('idxs', torch.tensor(idxs, device=module.weight.device))
         module.register_parameter('bf', torch.nn.Parameter(bias))
         module.register_buffer('zeros', torch.zeros(1, *bias.shape, dtype=bias.dtype, device=module.weight.device))
-
+        
         return module
-
+    
     def forward(self, x):
         x = super().forward(x)
-
-        #zeros = torch.zeros(x.shape[0], *self.bf.shape, dtype=x.dtype, device=self.weight.device)
+        
+        # zeros = torch.zeros(x.shape[0], *self.bf.shape, dtype=x.dtype, device=self.weight.device)
         zeros = self.zeros.expand(x.shape[0], *self.zeros.shape[1:])
         index = self.idxs[None, :, None, None].expand_as(x)
         expanded = torch.scatter(zeros, 1, index, x)
-
+        
         return expanded + self.bf
-
+    
     def __repr__(self):
         return f'ConvExpand({self.in_channels}, {self.out_channels}, exp={len(self.idxs)})'
 
@@ -46,7 +46,7 @@ class BatchNormB(nn.BatchNorm2d):
         module.__class__ = BatchNormB
         module.register_parameter('bf', torch.nn.Parameter(bias))
         return module
-
+    
     def forward(self, x):
         x = super().forward(x)
         return x + self.bf[:, None, None].expand_as(x[0])
@@ -56,24 +56,19 @@ class BatchNormExpand(nn.BatchNorm2d):
     @staticmethod
     def from_bn(module: nn.BatchNorm2d, idxs, bias, shape):
         module.__class__ = BatchNormExpand
-
-        module.register_buffer(
-            'idxs', torch.tensor(
-                idxs, device=module.weight.device))
+        
+        module.register_buffer('idxs', torch.tensor(idxs, device=module.weight.device))
         module.register_parameter('bf', torch.nn.Parameter(bias))
-        module.register_buffer('zeros', torch.zeros(1, 1, *shape[2:]))
-
+        module.register_buffer('zeros', torch.zeros(1, 1, *shape[2:], dtype=bias.dtype, device=module.weight.device))
+        
         return module
-
+    
     def forward(self, x):
         x = super().forward(x)
-
-        zeros = torch.zeros(x.shape[0],
-                            self.bf.shape[0],
-                            *x.shape[2:],
-                            dtype=x.dtype,
-                            device=self.weight.device)
+        
+        # zeros = torch.zeros(x.shape[0], self.bf.shape[0], *x.shape[2:], dtype=x.dtype, device=self.weight.device)
+        zeros = self.zeros.expand(x.shape[0], self.bf.shape[0], *self.zeros.shape[2:])
         index = self.idxs[None, :, None, None].expand_as(x)
         expanded = torch.scatter(zeros, 1, index, x)
-
+        
         return expanded + self.bf[:, None, None].expand_as(expanded)
