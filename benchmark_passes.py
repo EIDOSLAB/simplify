62f0db60fe7648598d836b66e85fb07dbe8e494e
Andrea Bragagnolo <andr.bragagnolo@gmail.com>
2021-07-08 21:39:51 +0200

    improved benchmark_passes.py timings

--- benchmark_passes.py
+++ benchmark_passes.py
@@ -1,23 +1,21 @@
 import time
 
-from torchvision.models.resnet import resnet101, resnet50
-import wandb
-
 import numpy as np
 import torch
 from matplotlib import pyplot as plt
 from torch import nn
 from torch.nn import CrossEntropyLoss
 from torch.nn.utils import prune
-from torchvision.models import resnet18
+from torchvision.models.resnet import resnet101
 from tqdm import tqdm
 
+import wandb
 from simplify import propagate, remove_zeroed
 from simplify.utils import get_pinned_out
 
 if __name__ == '__main__':
-    wandb.init()
-
+    # wandb.init()
+    
     network = resnet101
     model = network(True)
     device = torch.device("cuda")
@@ -72,27 +70,53 @@ if __name__ == '__main__':
         forward_time = []
         backward_time = []
         model.to(device)
-        for j in tqdm(range(11), desc="Pruned test"):
-            start = time.perf_counter()
-            output = model(fake_input)
-            forward_time.append(time.perf_counter() - start)
+        for j in tqdm(range(10), desc="Pruned test"):
+            if device == torch.device("cuda"):
+                starter, ender = torch.cuda.Event(enable_timing=True), torch.cuda.Event(enable_timing=True)
+                starter.record()
+            else:
+                start = time.perf_counter()
+            
+            output = model(fake_input)  # FORWARD PASS
+            
+            if device == torch.device("cuda"):
+                ender.record()
+                torch.cuda.synchronize()
+            else:
+                end = time.perf_counter()
+            
+            forward_time.append(starter.elapsed_time(ender) if device == torch.device("cuda") else end - start)
             
             loss = criterion(output, fake_target)
-            start = time.perf_counter()
-            loss.backward()
-            backward_time.append(time.perf_counter() - start)
+            
+            if device == torch.device("cuda"):
+                starter, ender = torch.cuda.Event(enable_timing=True), torch.cuda.Event(enable_timing=True)
+                starter.record()
+            else:
+                start = time.perf_counter()
+            
+            loss.backward()  # BACKWARD PASS
+            
+            if device == torch.device("cuda"):
+                ender.record()
+                torch.cuda.synchronize()
+            else:
+                end = time.perf_counter()
+            
+            backward_time.append(starter.elapsed_time(ender) if device == torch.device("cuda") else end - start)
+        
         forward_time = forward_time[1:]
         backward_time = backward_time[1:]
-
+        
         pruned_y_forward.append(np.mean(forward_time))
         pruned_y_forward_std.append(np.std(forward_time))
         pruned_y_backward.append(np.mean(backward_time))
         pruned_y_backward_std.append(np.std(backward_time))
-
+        
         wandb.log({
-            'pruned.forward': np.mean(forward_time),
-            'pruned.forward_std': np.std(forward_time),
-            'pruned.backward': np.mean(backward_time),
+            'pruned.forward':      np.mean(forward_time),
+            'pruned.forward_std':  np.std(forward_time),
+            'pruned.backward':     np.mean(backward_time),
             'pruned.backward_std': np.std(backward_time)
         }, commit=False)
         
@@ -104,15 +128,41 @@ if __name__ == '__main__':
         forward_time = []
         backward_time = []
         model.to(device)
-        for j in tqdm(range(11), desc="Simplified test"):
-            start = time.perf_counter()
-            output2 = model(fake_input)
-            forward_time.append(time.perf_counter() - start)
+        for j in tqdm(range(10), desc="Simplified test"):
+            if device == torch.device("cuda"):
+                starter, ender = torch.cuda.Event(enable_timing=True), torch.cuda.Event(enable_timing=True)
+                starter.record()
+            else:
+                start = time.perf_counter()
             
-            loss = criterion(output2, fake_target)
-            start = time.perf_counter()
-            loss.backward()
-            backward_time.append(time.perf_counter() - start)
+            output2 = model(fake_input)  # FORWARD PASS
+            
+            if device == torch.device("cuda"):
+                ender.record()
+                torch.cuda.synchronize()
+            else:
+                end = time.perf_counter()
+            
+            forward_time.append(starter.elapsed_time(ender) if device == torch.device("cuda") else end - start)
+            
+            loss2 = criterion(output2, fake_target)
+            
+            if device == torch.device("cuda"):
+                starter, ender = torch.cuda.Event(enable_timing=True), torch.cuda.Event(enable_timing=True)
+                starter.record()
+            else:
+                start = time.perf_counter()
+            
+            loss2.backward()  # BACKWARD PASS
+            
+            if device == torch.device("cuda"):
+                ender.record()
+                torch.cuda.synchronize()
+            else:
+                end = time.perf_counter()
+            
+            backward_time.append(starter.elapsed_time(ender) if device == torch.device("cuda") else end - start)
+        
         forward_time = forward_time[1:]
         backward_time = backward_time[1:]
         
@@ -120,36 +170,36 @@ if __name__ == '__main__':
         simplified_y_forward_std.append(np.std(forward_time))
         simplified_y_backward.append(np.mean(backward_time))
         simplified_y_backward_std.append(np.std(backward_time))
-
+        
         print(torch.equal(output.argmax(dim=1), output2.argmax(dim=1)))
         assert torch.equal(output.argmax(dim=1), output2.argmax(dim=1))
-
+        
         wandb.log({
-            'simplified.forward': np.mean(forward_time),
-            'simplified.forward_std': np.std(forward_time),
-            'simplified.backward': np.mean(backward_time),
+            'simplified.forward':      np.mean(forward_time),
+            'simplified.forward_std':  np.std(forward_time),
+            'simplified.backward':     np.mean(backward_time),
             'simplified.backward_std': np.std(backward_time),
-            'remaining_neurons': 100.-x[-1]
+            'remaining_neurons':       100. - x[-1]
         })
         
         amount += 0.05
         del model
         torch.cuda.empty_cache()
     
-    plt.errorbar(x, pruned_y_forward, pruned_y_forward_std, label="pruned")
-    plt.errorbar(x, simplified_y_forward, simplified_y_forward_std, label="simplified")
-    plt.title("Forward")
-    plt.xlabel("Pruning (%)")
-    plt.ylabel("Time (s)")
-    plt.legend()
-    plt.savefig("Forward.png", dpi=300)
-    plt.clf()
-    
-    plt.errorbar(x, pruned_y_backward, pruned_y_backward_std, label="pruned")
-    plt.errorbar(x, simplified_y_backward, simplified_y_backward_std, label="simplified")
-    plt.title("Backward")
-    plt.xlabel("Pruning (%)")
-    plt.ylabel("Time (s)")
-    plt.legend()
-    plt.savefig("Backward.png", dpi=300)
-    plt.clf()
+    # plt.errorbar(x, pruned_y_forward, pruned_y_forward_std, label="pruned")
+    # plt.errorbar(x, simplified_y_forward, simplified_y_forward_std, label="simplified")
+    # plt.title("Forward")
+    # plt.xlabel("Pruning (%)")
+    # plt.ylabel("Time (s)")
+    # plt.legend()
+    # plt.savefig("Forward.png", dpi=300)
+    # plt.clf()
+    #
+    # plt.errorbar(x, pruned_y_backward, pruned_y_backward_std, label="pruned")
+    # plt.errorbar(x, simplified_y_backward, simplified_y_backward_std, label="simplified")
+    # plt.title("Backward")
+    # plt.xlabel("Pruning (%)")
+    # plt.ylabel("Time (s)")
+    # plt.legend()
+    # plt.savefig("Backward.png", dpi=300)
+    # plt.clf()
