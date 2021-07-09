a87c9f3292f4179a7a44848349eee900353c6606
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2021-07-09 14:27:00 +0200

    add fuse_bn arg

--- simplify/__init__.py
+++ simplify/__init__.py
@@ -12,12 +12,14 @@ from .remove import remove_zeroed
 __version__ = "1.0.1"
 
 
-def simplify(model: nn.Module, x: torch.Tensor,
-             bn_folding: List = None) -> nn.Module:
-    if bn_folding is None:
-        bn_folding = []
+def simplify(model: nn.Module, x: torch.Tensor, fuse_bn=True, bn_folding: List = None) -> nn.Module:
+    
+    if fuse_bn:
+        if bn_folding is None:
+            bn_folding = []
+        
+        fuse(model, bn_folding)
 
-    fuse(model, bn_folding)
     pinned_out = utils.get_pinned_out(model)
     propagate_bias(model, x, pinned_out)
     remove_zeroed(model, x, pinned_out)
