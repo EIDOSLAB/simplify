ef8f0e7d06308def89acc6970b7add33f76b80e5
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2021-07-09 14:14:00 +0200

    tidy up train.py

--- training/train.py
+++ training/train.py
@@ -59,42 +59,32 @@ def main(config):
     torch.backends.cudnn.benchmark = False
     device = torch.device('cuda')
 
-    model = resnet18(False).to(device)
+    train_iteration = 10000
+    prune_iteration = config.prune_every
+
+    model = resnet50(False).to(device)
     for module in model.modules():
         if isinstance(module, nn.ReLU):
             module.inplace = False
 
-    #bn_folding = simplify.utils.get_bn_folding(model)
-    #model = simplify.fuse(model, bn_folding)
-    pinned_out = get_pinned_out(model)
-
-    train_iteration = 10000
-    prune_iteration = config.prune_every
-    test_iteration = 1000
-
-    train_loader, test_loader = get_data_loaders(os.path.join(
-        config.root, "ImageNet"), 256, 256, 0, True, 8, False)
     optimizer = SGD(model.parameters(), lr=0.1, weight_decay=1e-4)
-    scaler = torch.cuda.amp.GradScaler()
-
     scheduler = CosineAnnealingLR(optimizer, train_iteration, 1e-3)
     criterion = CrossEntropyLoss()
 
     total_neurons = 0
     remaining_neurons = 0
-    for name, module in model.named_modules():
+    for module in model.modules():
         if isinstance(module, nn.Conv2d):
             total_neurons += module.weight.shape[0]
             remaining_neurons += module.weight.shape[0]
 
     wandb.init(config=config)
-    wandb.watch(model)
+    # wandb.watch(model)
 
     # warmup
     # model(torch.randn(256, 3, 224, 224, device=device))
 
-    profiled = profile_model(model, torch.randn(
-        (256, 3, 224, 224), device=device), rows=1000)
+    profiled = profile_model(model, torch.randn((256, 3, 224, 224), device=device), rows=1000)
     with open('profile.txt', 'w') as f:
         f.write('\n\n -- THRESHOLDED --\n')
         f.write(profiled)
@@ -103,57 +93,48 @@ def main(config):
     num_samples = 0
     num_correct = 0
 
-    amp = False
-
-    # for i, (images, target) in enumerate(tqdm(train_loader)):
     for i in tqdm(range(train_iteration)):
         images = torch.randn((256, 3, 224, 224), device=device)
         target = torch.randint(0, 100, (256,), device=device)
-
-        model.train()
-        #images, target = images.to(device), target.to(device)
 
         # Prune the network by 5% at each pass
         if (i + 1) % prune_iteration == 0:
             print("Pruning")
 
             remaining_neurons = 0
-            for name, module in model.named_modules():
+            for module in model.modules():
                 if isinstance(module, nn.Conv2d):
-                    prune.ln_structured(
-                        module, 'weight', amount=0.05, n=2, dim=0)
+                    prune.ln_structured(module, 'weight', amount=0.05, n=2, dim=0)
                     ch_sum = module.weight.sum(dim=(1, 2, 3))
                     remaining_neurons += ch_sum[ch_sum != 0].shape[0]
 
                     if config.simplify:
                         prune.remove(module, 'weight')
 
-            print(
-                f"The current model has {(remaining_neurons / total_neurons) * 100} % of the original neurons")
+            print(f"The current model has {(remaining_neurons / total_neurons) * 100} % of the original neurons")
 
             if config.simplify:
                 print("Simplifying model")
-                #model = model.to("cpu")
                 model.eval()
+                pinned_out = get_pinned_out(model)
                 simplify.propagate_bias(model, torch.zeros(1, 3, 224, 224, device=device), pinned_out)
                 simplify.remove_zeroed(model, torch.ones(1, 3, 224, 224, device=device), pinned_out)
                 model.train()
-                #model = model.to(device)
 
                 profiled = profile_model(model, torch.randn((256, 3, 224, 224), device=device), rows=1000)
                 with open('profile.txt', 'a') as f:
-                    f.write(
-                        f'\n\n -- SIMPLIFIED {(remaining_neurons / total_neurons) * 100} --\n')
+                    f.write(f'\n\n -- SIMPLIFIED {(remaining_neurons / total_neurons) * 100} --\n')
                     f.write(profiled)
                 torch.cuda.empty_cache()
 
-            optimizer = SGD(model.parameters(), lr=0.1, weight_decay=1e-4)
-            scheduler = CosineAnnealingLR(
-                optimizer, train_iteration, 1e-3, last_epoch=-1)
-            for _ in range(i):
-                scheduler.step()
+                # Re-init optimizer and scheduler
+                optimizer = SGD(model.parameters(), lr=0.1, weight_decay=1e-4)
+                scheduler = CosineAnnealingLR(optimizer, train_iteration, 1e-3, last_epoch=-1)
+                for _ in range(i):
+                    scheduler.step()
 
-        with torch.cuda.amp.autocast(amp):
+        model.train()
+        with torch.enable_grad():
             start = time.time()
             output = model(images)
             forward_time = time.time() - start
@@ -162,37 +143,18 @@ def main(config):
             optimizer.zero_grad()
 
             start = time.time()
-            if amp:
-                scaler.scale(loss).backward()
-            else:
-                loss.backward()
+            loss.backward()
             backward_time = time.time() - start
-
-            _, predictions = output.max(1)
-            num_correct += (predictions == target).sum()
-            num_samples += predictions.size(0)
-
-        # Set to 0 the gradient of pruned neurons
-        """with torch.no_grad():
-            for name, module in model.named_modules():
-                if isinstance(module, nn.Conv2d):
-                    for n, p in module.named_parameters():
-                        if n == "weight_orig":
-                            p.grad.mul_(module.weight_mask)
-                        if n == "bias":
-                            p.grad.mul_(module.weight_mask[:, 0, 0, 0])"""
-
-        # what is the chance of an unpruned weight to be exactly 0?
+        
         for param in model.parameters():
-            param.grad.data.mul_(torch.abs(param.data) > 0)
-
-        if amp:
-            scaler.step(optimizer)
-            scaler.update()
-            scheduler.step()
-        else:
-            optimizer.step()
+            param.grad.data.mul_(torch.abs(param.data) > 0)        
+        
+        optimizer.step()
         optimizer.zero_grad()
+
+        _, predictions = output.max(1)
+        num_correct += (predictions == target).sum()
+        num_samples += predictions.size(0)
 
         to_log = {
             "Remaining neurons": (remaining_neurons / total_neurons),
@@ -201,30 +163,20 @@ def main(config):
             "Backward Time": backward_time,
             "epoch": i
         }
-
-        if (i + 1) % test_iteration == 0:
-            # Test
-            model.eval()
-            accuracy = test(test_loader, model)
-            to_log["Test Accuracy"] = accuracy
-
+        
         current_lr = [group["lr"] for group in optimizer.param_groups]
 
         for j, lr in enumerate(current_lr):
             to_log[f"lr{j}"] = lr
 
         wandb.log(to_log)
-
+        
         if (i + 1) % train_iteration == 0:
             break
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--root',
-        type=str,
-        default=f'{os.path.expanduser("~")}/data')
     parser.add_argument('--prune_every', type=int, default=1000)
     parser.add_argument('--simplify', action='store_true')
     config = parser.parse_args()
