8f0f16ccc51c438f786e56de1ef3a18c0b077684
Andrea Bragagnolo <andr.bragagnolo@gmail.com>
2021-07-12 15:53:58 +0200

    no expand if training

--- simplify/__init__.py
+++ simplify/__init__.py
@@ -12,16 +12,20 @@ from .remove import remove_zeroed
 __version__ = "1.0.1"
 
 
-def simplify(model: nn.Module, x: torch.Tensor, fuse_bn=True, bn_folding: List = None) -> nn.Module:
-    
-    if fuse_bn:
+def simplify(model: nn.Module, x: torch.Tensor, fuse_bn: bool = True, bn_folding: List = None,
+             training: bool = False) -> nn.Module:
+    if fuse_bn or training:
         if bn_folding is None:
             bn_folding = utils.get_bn_folding(model)
-        
-        fuse(model, bn_folding)
-
+        if fuse_bn:
+            fuse(model, bn_folding)
+    
     pinned_out = utils.get_pinned_out(model)
+    
+    if training:
+        pinned_out = list(set([item for sublist in bn_folding for item in sublist] + pinned_out))
+        
     propagate_bias(model, x, pinned_out)
-    remove_zeroed(model, x, pinned_out)
-
+    remove_zeroed(model, x, pinned_out, training=training)
+    
     return model
