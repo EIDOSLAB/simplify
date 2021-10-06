b454ea0340266f5a43f1c1144d38efa7a85ac073
Andrea Bragagnolo <andr.bragagnolo@gmail.com>
2021-10-06 10:46:45 +0200

    fix training being slower?

--- simplify/__init__.py
+++ simplify/__init__.py
@@ -12,8 +12,12 @@ from .remove import remove_zeroed
 __version__ = "1.0.1"
 
 
-def simplify(model: nn.Module, x: torch.Tensor, fuse_bn: bool = True, bn_folding: List = None,
+def simplify(model: nn.Module, x: torch.Tensor, bn_folding: List = None, fuse_bn: bool = True,
              training: bool = False) -> nn.Module:
+    if training and fuse_bn:
+        print("Cannot fuse BatchNorm in training mode")
+        fuse_bn = False
+    
     if fuse_bn or training:
         if bn_folding is None:
             bn_folding = utils.get_bn_folding(model)
@@ -22,16 +26,16 @@ def simplify(model: nn.Module, x: torch.Tensor, fuse_bn: bool = True, bn_folding
     
     pinned_out = utils.get_pinned_out(model)
     
-    if training:
-        new_pinned_out = []
-        for pinned in pinned_out:
-            new_pinned_out.append(pinned)
-            for conv_bn in bn_folding:
-                if pinned in conv_bn:
-                    new_pinned_out.append(conv_bn[1] if fuse_bn else conv_bn[0])
-                    break
-        
-        pinned_out = new_pinned_out
+    # if training:
+    #     new_pinned_out = []
+    #     for pinned in pinned_out:
+    #         new_pinned_out.append(pinned)
+    #         for conv_bn in bn_folding:
+    #             if pinned in conv_bn:
+    #                 new_pinned_out.append(conv_bn[1] if fuse_bn else conv_bn[0])
+    #                 break
+    #
+    #     pinned_out = new_pinned_out
     
     propagate_bias(model, x, pinned_out)
     remove_zeroed(model, x, pinned_out, training=training)
