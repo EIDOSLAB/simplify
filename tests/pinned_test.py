79ef2473c6c131e009a3e1b59ceab99fc931b74b
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2021-10-07 10:51:05 +0200

    remove MNAS hardcoded layers.7

---
+++ tests/pinned_test.py
@@ -0,0 +1,20 @@
+import unittest
+
+import simplify
+import torchvision
+from natsort import natsorted
+
+class PinnedTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_pinned(self):
+        def pinned_subtest(architecture):
+            model = architecture()
+            pinned = simplify.utils.get_pinned(model)
+            print(f'{architecture.__name__}:', natsorted(pinned), '\n')
+        
+        pinned_subtest(torchvision.models.alexnet)
+        pinned_subtest(torchvision.models.resnet18)
+        pinned_subtest(torchvision.models.resnext101_32x8d)
+        pinned_subtest(torchvision.models.mnasnet1_0)
