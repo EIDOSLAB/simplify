838585e08210a1984395761e521920486c6261ed
Andrea Bragagnolo <andr.bragagnolo@gmail.com>
2022-01-24 15:03:50 +0100

    update copyright notice

--- simplify/remove.py
+++ simplify/remove.py
@@ -1,5 +1,4 @@
 #  Copyright (c) 2022 EIDOSLab. All rights reserved.
-#  This file is part of the EIDOSearch library.
 #  See the LICENSE file for licensing terms (BSD-style).
 
 from typing import List
