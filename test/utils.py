8b35c7b28169366a81f2059591bcb1d2b2c95ec3
Andrea Bragagnolo <andr.bragagnolo@gmail.com>
2022-07-16 10:58:39 +0200

    fewer test models and fixed missing return

--- test/utils.py
+++ test/utils.py
@@ -13,17 +13,17 @@ from torchvision.models.vgg import vgg19_bn, vgg11, vgg11_bn, vgg13, vgg13_bn, v
 
 models = [
     alexnet,
-    vgg11, vgg11_bn, vgg13, vgg13_bn, vgg16, vgg16_bn, vgg19, vgg19_bn,
-    resnet18, resnet34, resnet50, resnet101, resnet152,
-    squeezenet1_0, squeezenet1_1,
-    densenet121, densenet161, densenet169, densenet201,
+    vgg11, vgg11_bn,
+    resnet18, resnet50,
+    squeezenet1_0,
+    densenet121,
     inception_v3,
     googlenet,
-    shufflenet_v2_x0_5, shufflenet_v2_x1_0, shufflenet_v2_x1_5, shufflenet_v2_x2_0,
-    mobilenet_v2, mobilenet_v3_small, mobilenet_v3_large,
-    resnext50_32x4d, resnext101_32x8d,
-    wide_resnet50_2, wide_resnet101_2,
-    mnasnet0_5, mnasnet0_75, mnasnet1_0, mnasnet1_3,
+    shufflenet_v2_x0_5,
+    mobilenet_v2, mobilenet_v3_small,
+    resnext50_32x4d,
+    wide_resnet50_2,
+    mnasnet0_5, mnasnet1_0,
     densenet121
 ]
 
@@ -44,3 +44,5 @@ def get_model(architecture, arch):
         if isinstance(module, nn.Conv2d):
             prune.random_structured(module, 'weight', amount=0.8, dim=0)
             prune.remove(module, 'weight')
+
+    return model
