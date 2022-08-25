d8d2ab35c7b45e5640998e9dc103ebb400bc8f50
Andrea Bragagnolo <andr.bragagnolo@gmail.com>
2022-08-25 09:36:45 +0200

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
-__version__ = "1.1.3"
+__version__ = "1.1.4"
 
 setup(
     name='torch-simplify',
